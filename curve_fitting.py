3dba69527d824bbdd227781eebea801ae4b3bb93
Jaime Lopez Ruiz <jaime@lopez-ruiz.com>
2025-08-04 12:12:17 +0100

    Area working + Removed Double Voigt

--- curve_fitting.py
+++ curve_fitting.py
@@ -20,17 +20,13 @@ def bwf(x, amp, cen, wid, q):
     s = (x - cen) / wid
     return amp * ((1 + s / q) ** 2) / (1 + s**2)
 
-def double_voigt(x, amp1, amp2, cen, wid1, wid2):
-    """
-    A sum of two Voigt profiles sharing the same center.
-    amp1, wid1: amplitude and width of the narrower Voigt
-    amp2, wid2: amplitude and width of the broader Voigt
-    cen: shared center position (cm⁻¹)
-    """
-    profile1 = true_voigt(x, amp1, cen, wid1)
-    profile2 = true_voigt(x, amp2, cen, wid2)
-    return profile1 + profile2
 
+def fwhm_voigt(wid):
+    gamma = wid / 2
+    sigma = wid / (2 * np.sqrt(2 * np.log(2)))
+    fwhm_g = 2.3548 * sigma
+    fwhm_l = 2 * gamma
+    return 0.5346 * fwhm_l + np.sqrt(0.2166 * fwhm_l**2 + fwhm_g**2)
 
 # === Regional Fitting Function ===
 def fit_peaks_regionwise(x_full, y_full, regions, center_tolerance=30):
@@ -57,14 +53,6 @@ def fit_peaks_regionwise(x_full, y_full, regions, center_tolerance=30):
                 ub += [2 * amp, cen + center_tolerance, 100, 100]
                 param_counts.append(4)
 
-            elif model_type == "double_voigt":
-                amp1, amp2, cen, wid1, wid2 = peak_def[1:]
-                # Shared tolerance on amplitude difference
-                init += [amp1, amp2, cen, wid1, wid2]
-                lb += [0, 0, cen - center_tolerance, 1, 1]
-                ub += [2 * amp1, 2 * amp2, cen + center_tolerance, 100, 100]
-                param_counts.append(5)  
-
 
             else:
                 amp, cen, wid = peak_def[1:]
@@ -84,13 +72,10 @@ def fit_peaks_regionwise(x_full, y_full, regions, center_tolerance=30):
             offset = 0
             for peak_def, count in zip(peak_defs, param_counts):
                 shape = peak_def[0]
+
                 if shape == "bwf":
                     amp, cen, wid, q = params[offset:offset+4]
                     y += bwf(x, amp, cen, wid, q)
-
-                elif shape == "double_voigt":
-                    amp1, amp2, cen, wid1, wid2 = params[offset:offset+5]
-                    y += true_voigt(x, amp1, cen, wid1) + true_voigt(x, amp2, cen, wid2)
 
                 else:
                     amp, cen, wid = params[offset:offset+3]
@@ -113,35 +98,8 @@ def fit_peaks_regionwise(x_full, y_full, regions, center_tolerance=30):
             if shape == "bwf":
                 amp, cen, wid, q = popt[offset:offset+4]
                 y_peak = bwf(x_full, amp, cen, wid, q)
-                fwhm = 2 * abs(wid)
+                fwhm = 2 * abs(wid) * np.sqrt(1 + 1 / q**2)
                 area = np.trapz(y_peak, x_full)
-
-            elif shape == "double_voigt":
-                amp1, amp2, cen, wid1, wid2 = popt[offset:offset+5]
-                y_voigt1 = true_voigt(x_full, amp1, cen, wid1)
-                y_voigt2 = true_voigt(x_full, amp2, cen, wid2)
-
-                fitted_peaks.append((x_full, y_voigt1))
-                peak_params.append({
-                    "peak": peak_counter,
-                    "model": "double_voigt_narrow",
-                    "mu": cen,
-                    "FWHM": 2.3548 * abs(wid1),
-                    "Area": amp1 * wid1 * np.sqrt(2 * np.pi),
-                    "Relative_Intensity": np.max(y_voigt1)
-                })
-                peak_counter += 1
-
-                fitted_peaks.append((x_full, y_voigt2))
-                peak_params.append({
-                    "peak": peak_counter,
-                    "model": "double_voigt_broad",
-                    "mu": cen,
-                    "FWHM": 2.3548 * abs(wid2),
-                    "Area": amp2 * wid2 * np.sqrt(2 * np.pi),
-                    "Relative_Intensity": np.max(y_voigt2)
-                })
-                peak_counter += 1
 
             else:
                 amp, cen, wid = popt[offset:offset+3]
@@ -155,23 +113,22 @@ def fit_peaks_regionwise(x_full, y_full, regions, center_tolerance=30):
                     area = amp * np.pi * wid
                 elif shape == "pvoigt" or shape == "voigt":
                     y_peak = true_voigt(x_full, amp, cen, wid)
-                    fwhm = wid
-                    area = amp
+                    fwhm = fwhm_voigt(wid)
+                    area = np.trapz(y_peak, x_full)
 
             offset += count
 
-            # ONLY append here for non-double_voigt cases
-            if shape != "double_voigt":
-                fitted_peaks.append((x_full, y_peak))
-                peak_params.append({
-                    "peak": peak_counter,
-                    "model": shape,
-                    "mu": cen,
-                    "FWHM": fwhm,
-                    "Area": area,
-                    "Relative_Intensity": np.max(y_peak)
-                })
-                peak_counter += 1
+            fitted_peaks.append((x_full, y_peak))
+            peak_params.append({
+                "peak": peak_counter,
+                "model": shape,
+                "mu": cen,
+                "FWHM": fwhm,
+                "Area": area,
+                "Relative_Intensity": np.max(y_peak)
+            })
+            
+            peak_counter += 1
 
     return y_fit_total, fitted_peaks, peak_params
 
--- main.py
+++ main.py
@@ -11,8 +11,12 @@ from analysis_plotting import plot_and_report
 # === Region Definitions ===
 # Format: (start, end, [ (model, amp, center, width), ... ])
 REGIONS = [
-    (170, 300, [("voigt",0.1,186,10),("voigt",0.1,266,10)]),
-    (380, 1450, [("voigt", 0.1, 435, 5),("voigt", 0.2, 500, 5),("voigt", 0.2, 535, 5),("voigt", 0.1, 660, 1),("voigt", 0.1, 660, 1),("lorentz", 0.4, 767, 1), ("lorentz", 0.4, 790, 1),("lorentz", 0.4, 795, 1),("gauss",0.5,770,5), ("voigt",0.3, 849, 2),("voigt", 0.3, 940, 2),("voigt", 0.3, 923, 2),("gauss", 0.3, 870, 10),("voigt", 0.3, 870, 2),("bwf",0.5,1250,5,2),("gauss",0.5,1080,5),("lorentz",0.1,1405,2)]),
+    (170, 300, [("voigt", 0.1, 186, 10), ("voigt", 0.1, 266, 10)]),
+    (380, 1540, [("voigt", 0.1, 435, 5), ("voigt", 0.15, 500, 5), ("lorentz", 0.15, 520, 5), ("voigt", 0.15, 600, 5), 
+                 ("voigt", 0.15, 660, 5),
+                 ("lorentz", 0.4, 767, 1), ("lorentz", 0.4, 790, 1),("lorentz", 0.4, 795, 1),("gauss",0.5,770,5), 
+                 ("voigt",0.3, 849, 2), ("voigt", 0.3, 940, 2),("voigt", 0.3, 923, 2), ("gauss", 0.3, 870, 10), ("voigt", 0.3, 870, 2),
+                 ("gauss", 0.5, 1080, 5), ("bwf", 0.5, 1250, 5, 2), ("lorentz", 0.1, 1405, 2), ("voigt", 0.07, 1520, 2)]),
     #(1350, 1450, [("lorentz",0.1,1405,2)])
 ]
 # === File Input Handling ===
